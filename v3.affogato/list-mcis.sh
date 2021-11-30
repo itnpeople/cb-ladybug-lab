ec59186febfda18922be258cf73243a6fe9b0ef3
escho <itnpeople@naver.com>
2021-11-30 09:51:59 +0900

    affogato

--- v3.affogato/list-mcis.sh
+++ v3.affogato/list-mcis.sh
@@ -11,5 +11,6 @@ echo "- Namespace                  is '${c_NS}'"
 
 # ------------------------------------------------------------------------------
 # show init result
- curl -sX GET ${c_URL_TUMBLEBUG_NS}/mcis -H "${c_AUTH}" -H "${c_CT}" | jq;
+curl -sX GET ${c_URL_TUMBLEBUG_NS}/resources/securityGroup	-H "${c_AUTH}" 	-H "${c_CT}" | jq;
+#curl -sX GET ${c_URL_TUMBLEBUG_NS}/mcis -H "${c_AUTH}" -H "${c_CT}" | jq;
 
